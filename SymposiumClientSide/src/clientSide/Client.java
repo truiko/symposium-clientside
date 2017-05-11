db3ccd2c4ac1b0e259f69ca3cf917c1e148d8f5b
Student 8 <Student 8@10.8.33.94>
2017-05-11 13:41:35 -0400

    make client constructor

--- SymposiumClientSide/src/clientSide/Client.java
+++ SymposiumClientSide/src/clientSide/Client.java
@@ -1,9 +1,39 @@
 package clientSide;
 
-public class Client {
+import java.io.*;
+import java.net.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-	public Client() {
-		// TODO Auto-generated constructor stub
+public class Client extends JFrame{
+
+	private JTextField userText;
+	private JTextArea chatWindow;
+	private ObjectOutputStream output;
+	private ObjectInputStream input;
+	private String message = "";
+	private String serverIP;
+	private Socket connection;
+	
+	public Client(String host) {
+		super("Symposium Client");
+		serverIP = host;
+		userText = new JTextField();
+		userText.setEditable(false);
+		userText.addActionListener(
+			new ActionListener(){
+				public void actionPerformed(ActionEvent event){
+					sendData(event.getActionCommand());
+					userText.setText("");
+				}
+			}
+		);
+		add(userText, BorderLayout.NORTH);
+		chatWindow = new JTextArea();
+		add(new JScrollPane(chatWindow), BorderLayout.CENTER);
+		setSize(300,150);
+		setVisible(true);
 	}
 
 }
