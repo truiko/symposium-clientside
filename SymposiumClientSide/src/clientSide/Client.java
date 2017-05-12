7782acda3b2d32efa94b1245c13a13e5d503c7db
Student 8 <Student 8@10.8.33.94>
2017-05-12 13:53:30 -0400

    finished client program

--- SymposiumClientSide/src/clientSide/Client.java
+++ SymposiumClientSide/src/clientSide/Client.java
@@ -53,5 +53,74 @@ public class Client extends JFrame{
 	//connect to server
 	public void connectToServer() throws IOException{
 		showMessage("Attempting connection... \n");
+		connection = new Socket(InetAddress.getByName(serverIP), 6789);
+		showMessage("Connected to: " + connection.getInetAddress().getHostName());
+	}
+	
+	//set up streams to send and receive messages
+	private void setupStreams() throws IOException{
+		output = new ObjectOutputStream(connection.getOutputStream());
+		output.flush();
+		input = new ObjectInputStream(connection.getInputStream());
+		showMessage("\n Streams are now good to go! \n");
+	}
+	
+	//while chatting with server
+	private void whileChatting() throws IOException{
+		ableToType(true);
+		do{
+			try{
+				message = (String) input.readObject();
+				showMessage("\n" + message);
+			}catch(ClassNotFoundException classNotFoundException){
+				showMessage("\n I don't know that object type");
+			}
+		}while(!message.equals("SERVER - END"));
+	}
+	
+	//close the streams and sockets
+	private void closeAll(){
+		showMessage("\n Closing all down... ");
+		ableToType(false);
+		try{
+			output.close();
+			input.close();
+			connection.close();
+		}catch(IOException ioException){
+			ioException.printStackTrace();
+		}
+	}
+	
+	//send messages to the server
+	private void sendMessage(String message){
+		try{
+			output.writeObject("CLIENT - " + message);
+			output.flush();
+			showMessage("\nCLIENT -" + message);
+		}catch(IOException ioException){
+			chatWindow.append("\n Something went wrong when sending message!");
+		}
+	}
+	
+	// change/update chatWindow
+	private void showMessage(final String m){
+		SwingUtilities.invokeLater(
+			new Runnable(){
+				public void run(){
+					chatWindow.append(m);
+				}
+			}
+		);
+	}
+	
+	//gives user permission to type into the text box
+	private void ableToType(final boolean tof){
+		SwingUtilities.invokeLater(
+			new Runnable(){
+				public void run(){
+					userText.setEditable(tof);	
+				}
+			}
+		);		
 	}
 }
