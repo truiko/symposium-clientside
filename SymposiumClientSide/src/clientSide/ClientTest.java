1c591edfe37d69aff2c2ef1c4fbf25fc069e9f08
Student 8 <Student 8@10.8.33.94>
2017-05-12 14:02:42 -0400

    finished clientTest class

--- SymposiumClientSide/src/clientSide/ClientTest.java
+++ SymposiumClientSide/src/clientSide/ClientTest.java
@@ -1,9 +1,14 @@
 package clientSide;
 
+import javax.swing.JFrame;
+
 public class ClientTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Client client;
+		client = new Client("127.0.0.1");
+		client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		client.startRunning();
 
 	}
 
