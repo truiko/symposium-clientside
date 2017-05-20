1934dc7006be345eafdabe53791aee3fa56692b2
Veeraj Jethalal <veera@DESKTOP-0ECFG49>
2017-05-20 19:52:52 -0400

    Almost Done With Login

--- SymposiumClientSide/src/clientSide/ClientTest.java
+++ SymposiumClientSide/src/clientSide/ClientTest.java
@@ -6,9 +6,11 @@ public class ClientTest {
 
 	public static void main(String[] args) {
 		Client client;
-		client = new Client("127.0.0.1");
-		client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		client.startRunning();
+		Login login;
+		login = new Login();
+		//client = new Client("127.0.0.1");
+		//client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//client.startRunning();
 
 	}
 
--- SymposiumClientSide/src/clientSide/Login.java
+++ SymposiumClientSide/src/clientSide/Login.java
@@ -1,33 +1,124 @@
 package clientSide;
 
-import java.io.*;
-import java.net.*;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.*;
+import java.io.*;
+import java.util.*;
 
-public class Login extends JFrame{
+public class Login extends JFrame {
+  JButton login;
+  JPanel loginpanel;
+  JTextField txuser;
+  JTextField pass;
+  JButton newUser;
+  JLabel username;
+  JLabel password;
 
-	private JTextArea window;
-	private JTextField username;
-	private JTextField password;
-	private JButton login;
-	
-	public Login() {
-		super("Login Information");
-		username = new JTextField();
-		password = new JTextField();
-		window = new JTextArea();
-		login = new JButton();
-		login.setText("Login");
-		add(username, BorderLayout.NORTH);
-		add(new JScrollPane(window), BorderLayout.CENTER);
-		setSize(300,150);
-		setVisible(true);
-	}
-	
-	public static void main(String[] args){
-		new Login();
-	}
+
+  public Login(){
+    super("Login Screen");
+
+    login = new JButton("Login");
+    loginpanel = new JPanel();
+    txuser = new JTextField(15);
+    pass = new JPasswordField(15);
+    newUser = new JButton("New User?");
+    username = new JLabel("User - ");
+    password = new JLabel("Pass - ");
+
+    setSize(300,200);
+    setLocation(500,280);
+    loginpanel.setLayout (null); 
+
+
+    txuser.setBounds(70,30,150,20);
+    pass.setBounds(70,65,150,20);
+    login.setBounds(110,100,80,20);
+    newUser.setBounds(110,135,80,20);
+    username.setBounds(20,28,80,20);
+    password.setBounds(20,63,80,20);
+
+    loginpanel.add(login);
+    loginpanel.add(txuser);
+    loginpanel.add(pass);
+    loginpanel.add(newUser);
+    loginpanel.add(username);
+    loginpanel.add(password);
+
+    getContentPane().add(loginpanel);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setVisible(true);
+
+    Writer writer = null;
+    File check = new File("userPass.txt");
+    if(check.exists()){
+
+      //Checks if the file exists. will not add anything if the file does exist.
+    }else{
+      try{
+        File texting = new File("userPass.txt");
+        writer = new BufferedWriter(new FileWriter(texting));
+        writer.write("message");
+      }catch(IOException e){
+        e.printStackTrace();
+      }
+    }
+
+
+
+
+    login.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        try {
+          File file = new File("userPass.txt");
+          Scanner scan = new Scanner(file);;
+          String line = null;
+          FileWriter filewrite = new FileWriter(file, true);
+
+          String usertxt = " ";
+          String passtxt = " ";
+          String puname = txuser.getText();
+          String ppaswd = pass.getText();
+
+
+          while (scan.hasNext()) {
+            usertxt = scan.nextLine();
+            passtxt = scan.nextLine();
+
+          }
+
+
+
+
+          if(puname.equals(usertxt) && ppaswd.equals(passtxt)) {
+            Menu menu =new Menu();
+            dispose();
+          } 
+          else if(puname.equals("") && ppaswd.equals("")){
+            JOptionPane.showMessageDialog(null,"Please insert Username and Password");
+          }
+          else {
+
+            JOptionPane.showMessageDialog(null,"Wrong Username / Password");
+            txuser.setText("");
+            pass.setText("");
+            txuser.requestFocus();
+          }
+        } catch (IOException d) {
+          d.printStackTrace();
+        }
+
+      }
+    });
+
+    newUser.addActionListener(new ActionListener(){
+      public void actionPerformed(ActionEvent e) {
+        NewUser user = new NewUser();
+        dispose();
+
+      }
+    });
+  } 
 
 }
