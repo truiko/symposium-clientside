93e0be745b699f43b60fbf297c927e4915079e4b
sidings <jrna.fng@gmail.com>
2017-05-24 13:27:30 -0400

    cool

--- SymposiumClientSide/src/clientSide/ClientTest.java
+++ SymposiumClientSide/src/clientSide/ClientTest.java
@@ -6,7 +6,7 @@ public class ClientTest {
 
 	public static void main(String[] args) {
 		Client client;
-		client = new Client("127.0.0.1");
+		client = new Client("10.8.33.1");//64.71.171.62
 		client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		client.startRunning();
 
