8e624d8d8a46713d5ed0132a3c94441935e9307b
Student 8 <Student 8@10.8.33.94>
2017-05-24 13:57:01 -0400

    trying to build micThread class

--- SymposiumClientSide/src/clientSide/MicThread.java
+++ SymposiumClientSide/src/clientSide/MicThread.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.zip.GZIPOutputStream;
 import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
@@ -24,49 +25,43 @@ public class MicThread extends Thread {
     public static double amplification = 1.0;
     private ObjectOutputStream toServer;
     private TargetDataLine mic;
+    private AudioFormat format;
 
     public MicThread(ObjectOutputStream toServer) throws LineUnavailableException {
         this.toServer = toServer;
-        //open microphone line, an exception is thrown in case of error
-        AudioFormat af = SoundPacket.defaultFormat;
-        DataLine.Info info = new DataLine.Info(TargetDataLine.class, af);
-        mic = (TargetDataLine) (AudioSystem.getLine(info));
-        mic.open(af);
-        mic.start();
+        format = new AudioFormat(11025f, 8, 1, true, true);
+        DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+        // checks if system supports the data line
+        if (!AudioSystem.isLineSupported(info)) {
+        	System.out.println("Line not supported");
+            System.exit(0);
+        }
+        
+        mic = (TargetDataLine) AudioSystem.getLine(info);
+        mic.open(format);
+        mic.start();   // start capturing
     }
 
     @Override
     public void run() {
         for (;;) {
-            if (mic.available() >= SoundPacket.defaultDataLength) { //we got enough data to send
-                byte[] buff = new byte[SoundPacket.defaultDataLength];
-                while (mic.available() >= SoundPacket.defaultDataLength) { //flush old data from mic to reduce lag, and read most recent data
+            if (mic.available() >= 1200) { //we got enough data to send
+                byte[] buff = new byte[1200];
+                while (mic.available() >= 1200) { //flush old data from mic to reduce lag, and read most recent data
                     mic.read(buff, 0, buff.length); //read from microphone
                 }
                 try {
-                    //this part is used to decide whether to send or not the packet. if volume is too low, an empty packet will be sent and the remote client will play some comfort noise
-                    long tot = 0;
-                    for (int i = 0; i < buff.length; i++) {
-                        buff[i] *= amplification;
-                        tot += Math.abs(buff[i]);
-                    }
-                    tot *= 2.5;
-                    tot /= buff.length;
                     //create and send packet
                     Message m = null;
-                    if (tot == 0) {//send empty packet
-                        m = new Message(-1, -1, new SoundPacket(null));
-                    } else { //send data
-                        //compress the sound packet with GZIP
-                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                        GZIPOutputStream go = new GZIPOutputStream(baos);
-                        go.write(buff);
-                        go.flush();
-                        go.close();
-                        baos.flush();
-                        baos.close();
-                        m = new Message(-1, -1, new SoundPacket(baos.toByteArray()));  //create message for server, will generate chId and timestamp from this computer's IP and this socket's port 
-                    }
+                    //compress the sound packet with GZIP
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    GZIPOutputStream go = new GZIPOutputStream(baos);
+                    go.write(buff);
+                    go.flush();
+                    go.close();
+                    baos.flush();
+                    baos.close();
+                    m = new Message(-1, -1, new SoundPacket(baos.toByteArray()));  //create message for server, will generate chId and timestamp from this computer's IP and this socket's port 
                     toServer.writeObject(m); //send message
                 } catch (IOException ex) { //connection error
                 	ex.printStackTrace();
