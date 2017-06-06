a8db7669bf8f7e7873342b95a7f93fee28e738d8
Student 8 <Student 8@10.8.33.156>
2017-06-06 13:53:39 -0400

    using bytes

--- SymposiumClientSide/src/clientSide/Client.java
+++ SymposiumClientSide/src/clientSide/Client.java
@@ -102,13 +102,22 @@ public class Client extends JFrame{
 	private void whileChatting() throws IOException{
 		ableToType(true);
 		do{
-			try{
-				//message = (String) input.readObject();
-				message = (new Message(input.readObject()));
-				showMessage("\n" + message.getData());
-			}catch(ClassNotFoundException classNotFoundException){
-				showMessage("\n I don't know that object type");
-			}
+			//if(message.getData() instanceof String){
+				try{
+					//message = (String) input.readObject();
+					message = (new Message(input.readObject()));
+					showMessage("\n" + message.getData());
+				}catch(ClassNotFoundException classNotFoundException){
+					showMessage("\n I don't know that object type");
+				}
+			//}
+//			else{
+//				if(message.getData() instanceof BufferedImage){
+//					try{
+//						message = (new Message(input.readObject()));	
+//					}
+//				}
+//			}
 		}while(!message.getData().equals("SERVER - END"));
 	}
 	
@@ -170,6 +179,7 @@ public class Client extends JFrame{
 		//try (ServerSocket serv = new ServerSocket(25000)) {
 		   //  System.out.println("waiting...");
 		    //  try (Socket socket = serv.accept()) {
+		
 		BufferedImage img = null;
 		JFileChooser fc = new JFileChooser();
 		int returnVal = fc.showOpenDialog(fc);
@@ -180,62 +190,64 @@ public class Client extends JFrame{
 			System.out.println("got filepath");
 		}else{
 			System.out.println("User clicked CANCEL");
-			System.exit(1);//
+			System.exit(1);
 		}
-		try{
-			img = ImageIO.read(new File(filePath));
-			System.out.println("made img variable");
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-        ImageIO.write(img, "jpg", output);
-        output.writeObject(null);
-        //output.flush();
-        System.out.println("sent");
+//		try{
+//			img = ImageIO.read(new File(filePath));
+//			System.out.println("made img variable");
+//		}catch(Exception e){
+//			e.printStackTrace();
+//		}
+//        ImageIO.write(img, "jpg", output);
+//        output.writeObject(null);
+//        //output.flush();
+//        System.out.println("sent");
+        
 		     // }
 		//}
         
-//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//        ImageIO.write(img, "jpg", baos);
-//        byte[] bytes = baos.toByteArray();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(img, "jpg", baos);
+        byte[] bytes = baos.toByteArray();
+        //output.writeObject(null);
         
-      /*  try {
-
-            byte[] imageInByte;
-            BufferedImage originalImage1 = ImageIO.read(new File(filePath));
-
-            // convert BufferedImage to byte array
-            ByteArrayOutputStream baos1 = new ByteArrayOutputStream();
-            ImageIO.write(originalImage1, "jpg", baos1);
-            baos1.flush();
-            byte[] ba1 = baos1.toByteArray();
-            imageInByte = new byte[ba1.length];
-            //System.out.println(new String(imageInByte));
-            System.arraycopy(ba1, 0, imageInByte, 0, ba1.length);
-            //System.out.println(new String(imageInByte));
-            //System.out.println(new String(imageInByte));
-            baos1.close();
-
-            // convert byte array back to BufferedImage
-            InputStream in = new ByteArrayInputStream(imageInByte);
-
-            int w = Math.max(originalImage1.getWidth(), w);
-            //int h = Math.max(originalImage1.getHeight(), originalImage2.getHeight());
-            int h = originalImage1.getHeight();
-            BufferedImage bImageFromConvert = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-            //BufferedImage bImageFromConvert = new BufferedImage(w, h, BufferedImage.TYPE_4BYTE_ABGR );
-
-            //BufferedImage bImageFromConvert = ImageIO.read(in);
-
-            Graphics g = bImageFromConvert.getGraphics();
-            g.drawImage(originalImage1, 0, 0, null);
-
-            ImageIO.write(bImageFromConvert, "jpg", new File("result.jpg"));
-
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-    }*/
+//        try {
+//
+//            byte[] imageInByte;
+//            BufferedImage originalImage1 = ImageIO.read(new File(filePath));
+//
+//            // convert BufferedImage to byte array
+//            ByteArrayOutputStream baos1 = new ByteArrayOutputStream();
+//            ImageIO.write(originalImage1, "jpg", baos1);
+//            baos1.flush();
+//            byte[] ba1 = baos1.toByteArray();
+//            imageInByte = new byte[ba1.length];
+//            //System.out.println(new String(imageInByte));
+//            System.arraycopy(ba1, 0, imageInByte, 0, ba1.length);
+//            //System.out.println(new String(imageInByte));
+//            //System.out.println(new String(imageInByte));
+//            baos1.close();
+//
+//            // convert byte array back to BufferedImage
+//            InputStream in = new ByteArrayInputStream(imageInByte);
+//
+//            int w = Math.max(originalImage1.getWidth(), w);
+//            //int h = Math.max(originalImage1.getHeight(), originalImage2.getHeight());
+//            int h = originalImage1.getHeight();
+//            BufferedImage bImageFromConvert = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+//            //BufferedImage bImageFromConvert = new BufferedImage(w, h, BufferedImage.TYPE_4BYTE_ABGR );
+//
+//            //BufferedImage bImageFromConvert = ImageIO.read(in);
+//
+//            Graphics g = bImageFromConvert.getGraphics();
+//            g.drawImage(originalImage1, 0, 0, null);
+//
+//            ImageIO.write(bImageFromConvert, "jpg", new File("result.jpg"));
+//
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage());
+//        }
+//    }
 	}
 	
 	private void receiveImage() throws IOException{
