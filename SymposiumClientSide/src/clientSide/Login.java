41c78a6d367fa1bcc2c553be37d57aad99619f53
joycefeng <joycefeng@joyces-mbp.fios-router.home>
2017-06-15 00:00:04 -0400

    login fix

--- SymposiumClientSide/src/clientSide/Login.java
+++ SymposiumClientSide/src/clientSide/Login.java
@@ -81,54 +81,48 @@ public class Login extends JFrame {
           			String ppaswd = pass.getText();
 
 
+          			if(puname.equals("") && ppaswd.equals("")){
+          				JOptionPane.showMessageDialog(null,"Please insert Username and Password");
+          			}
+          			boolean c = false;
           			while (scan.hasNext()) {
           				usertxt = scan.nextLine();
           				passtxt = scan.nextLine();
-
+						if(puname.equals(usertxt) && ppaswd.equals(passtxt)) {
+						    c = true;    
+							Client client;
+				  				try{
+						          	client = new Client("127.0.0.1");
+						          	client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+						            Thread go = new Thread(new Runnable() {
+											@Override
+											public void run() {
+												client.startRunning();
+											}
+										});
+						          	go.start();
+						          					
+						          					
+						          }catch(Exception ex){
+						          		ex.printStackTrace();
+						          }
+						          		setVisible(false);
+						          		break;
+						  }
           			}
-
-          			if(puname.equals(usertxt) && ppaswd.equals(passtxt)) {
-          				Client client;
-          				try{
-          					client = new Client("127.0.0.1");
-          					client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-          					Thread go = new Thread(new Runnable() {
-								
-								@Override
-								public void run() {
-				
-									client.startRunning();
-									
-								}
-							});
-          					go.start();
-          					
-          					
-          				}catch(Exception ex){
-          					ex.printStackTrace();
-          				}
-          				setVisible(false);
-          			} 
-          			else if(puname.equals("") && ppaswd.equals("")){
-          				JOptionPane.showMessageDialog(null,"Please insert Username and Password");
-          			}
-          			else {
-        	  
+          			if (c == false) {
           				JOptionPane.showMessageDialog(null,"Wrong Username / Password");
           				txuser.setText("");
           				pass.setText("");
           				txuser.requestFocus();
           			}
-
-
-
-				} catch (IOException d) {
+          			} catch (IOException d) {
 					d.printStackTrace();
 				}
 
 			}
 		});
-
+		
 		newUser.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
 				NewUser user = new NewUser();
